277f57123d8c4b50c4a5411d65a4523bbc72aed1
Xavier Verdejo <xavierverdejo@paucasesnovescifp.cat>
2020-11-05 20:08:55 +0100

    Created project SPDVIDebugging Created Main function and created Person class.

---
+++ SPDVIDebugging/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPDVIDebugging
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FunkyNameBuilder()
+        {
+            string output = "";
+            for(int i = 0; i<=FirstName.Length; i++)
+            {
+                if(i%2 == 0)
+                {
+                    output += FirstName.Substring(i, 1);
+                }
+                else
+                {
+                    output += FirstName.Substring(i, 1).ToUpper();
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ SPDVIDebugging/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SPDVIDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.FirstName = "Timothy";
+            p.LastName = "Corey";
+
+            Console.WriteLine(p.FunkyNameBuilder());
+            Console.ReadLine();
+        }
+    }
+}
